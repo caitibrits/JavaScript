642c130e5bf035cb7d4c89fb372e7708c0e728e6
Caitlin Brits <93997754+caitibrits@users.noreply.github.com>
2022-04-08 13:26:40 +0200

    Basic Arithmetic challenge

---
+++ javascript.js
@@ -0,0 +1,44 @@
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+
+   let rectangleLength = 10;
+   let rectangleBreadth = 17;
+   let circleRadius = 25;
+   let triangleBase = 20;
+   let triangleHeight = 9;
+
+
+// Rectangle area calculation
+
+let rectangleArea = rectangleLength * rectangleBreadth;
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : " + rectangleArea);
+
+
+console.log("\n");
+
+
+// circle area calculation
+
+let circleArea = circleRadius * circleRadius * pi;
+
+// Append your circle answer:
+
+console.log("The area of the given circle is : " + circleArea);
+
+
+console.log("\n");
+
+
+// triangle area calculation
+
+let triangleArea = triangleBase * triangleHeight;
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : " + triangleArea);
