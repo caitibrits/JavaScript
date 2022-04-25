9d5c54d420c2ed4fe808818587a75afdcaa493ca
caitibrits <caiti.tyers@gmail.com>
2022-04-25 11:13:51 +0200

    Done as much as I can on the validating side
    Haven't gotten the JS side of validation, still need guidance on it

--- javascriptforms.js
+++ javascriptforms.js
@@ -11,35 +11,12 @@ function checkInputs() {
   //get values from inputs
   const newTaskValue = newTask.value.trim();
 
-  if(newTaskValue === '') {
-    //show error
-    //add error class
-    setErrorFor(newTask, 'Please make sure your task contains no HTML characters');
-  } else {
-    //add success class
-    setSuccessFor(newTask);
-  }
-
-  if(newTaskValue === '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'); {
-    setErrorFor(newTask, 'Please make sure your task contains no HTML characters');
-  } 
+  if (newTask.includes("<" || ">" )) {
+    document.getElementById("newTask").style.display = "block";
+    return false;
+}
+else {
+    return true;
 }
 
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  //add error message inside small
-  small.innerText = message;
-
-  //add error class
-  formControl.className = 'form-control error';
-}
-
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control success';
-}
-
-function setErrorFor(newTask){
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
