36e96ecd6588004c187e55e0bfd7fcc304de1721
caitibrits <caiti.tyers@gmail.com>
2022-05-26 13:41:33 +0200

    Still figuring details out

--- userListJS.js
+++ userListJS.js
@@ -1,36 +1,47 @@
+let addUserButton = document.getElementById('addUser');
+let userContainer = document.getElementById('userContainer');
+let inputField = document.getElementById('inputField');
+
 let userList = [
     user1 = 'Gerrie',
     user2 = 'Susan',
     user3 = 'Geerhard',
     user4 = 'Caitlin',
     user5 = 'Eckhard',
-    user6 = 'Jaco',
-    user7 = 'Ane',
-    user8 = 'Petrie',
-    user9 = 'Janco',
-    user10 = 'Ane',
+    user6 = 'John',
+    user7 = 'Margie',
+    user8 = 'Jess',
+    user9 = 'Sarah',
+    user10 = 'Claire',
 ];
 
-let menuOptions = ['1', '2', '3', '4'];
+let menuOptions = [
+    addUser = '1', 
+    removeUser = '2', 
+    disableUser = '3', 
+    Sort = '4'
+];
 
-let userMenu = prompt('Choose an option:\n 1) Add three Users \n 2) Remove User \n 3) Display all users \n 4) Sort users \n');
+let choices = prompt('Choose an option:\n 1) Add three Users \n 2) Remove User \n 3) Display users \n 4) Sort users');
 
+// function options() {
 switch (menuOptions) {
     case '1':
+        addUserButton()
+            let li = document.createElement('li');
+            li.innerText = inputField.Value;
+            userContainer.appendChild(li);
+            console.log(inputField);
         break;
     case '2':
-        prompt('Who do you want to remove?');
-        userList.reduce(function(total, current));
+        li.addEventListener('click', function () {
+            li.removeChild(li);})
         break;
     case '3':
-        console.log('Display all users');
+        console.log(`${user1} ${user2} ${user3} ${user4} ${user5} ${user6} ${user7} ${user8} ${user9} ${user10}`)
         break;
     case '4':
-        console.log('Sort users');
+        userList.sort();
         break;
 }
-
-if (userMenu = () => ('1')) {
-       newUser = userList.map(function (user) {return user++;})
-        console.log('${item} will be added');
-};
+// 
