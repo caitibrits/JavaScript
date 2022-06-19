4f8a33c0e5b64c2f23a6d1b333d4aeb810f2471a
caitibrits <caiti.tyers@gmail.com>
2022-06-19 12:06:34 +0200

    Trying to add tasks to App

--- toDoApp.js
+++ toDoApp.js
@@ -1,67 +1,27 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector('#new-task-form');
-    const input = document.querySelector('#new-task-input');
-    const list_el = document.querySelector('#tasks');
+// get value of input from HTML and assign to variable
+let addInput = document.getElementById('addNewInput');
+// referrencing todos li
+const list = document.querySelector('.todos');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+// funtion to add tasks
+function getInput () {
+    //creating variable to assign input to
+    let todo;
+    // assigning input to variable
+    todo = addInput.value.trim();
+    //displaying todo
+    return(todo);
+};
 
-        const task = input.value;
-        
-        if(!task) {
-            alert('Please fill out the task');
-            return;
-        } 
-
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
-
-        task_el.appendChild(task_content_el);
-
-        const task_input_el = document.createElement("input");
-        task_content_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = task;
-        task_input_el.setAttribute("readonly", "readonly");
-
-        task_content_el.appendChild(task_input_el);
-
-        const task_actions_el = document.createElement("div");
-        task_actions_el.classList.add("actions");
-
-        const task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "Edit";
-
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "Delete";
-
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_actions_el);
-
-        list_el.appendChild(task_el);
-
-        input.value = "";
-
-        task_edit_el.addEventListener('click', () => {
-            if (task_edit_el.innerText.toLowerCase() === "edit") {
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText = "Save";
-            } else {
-                task_input_el.setAttribute("readonly", "readonly");
-                task_input_el.innerText = "Edit";
-            }
-        });
-
-        task_delete_el.addEventListener("click", () => {
-            list_el.removeChild(task_el);
-        });
-    });
-});
+// creating function to display HTML and output the todo inside it
+function display() {
+    document.getElementById('todo').innerHTML = "";
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        let ourDiv = document.createElement("div");
+        let html = `
+                <li>${todo}</li>
+                <button>Delete</button>
+            `;
+    };
+};
